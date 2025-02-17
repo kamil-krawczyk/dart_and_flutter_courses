a418bcdad0fb57bb27d9bfc435cd964047234dec
Kamil Krawczyk <kamil.krawczyk87@gmail.com>
2025-02-17 17:58:52 +0100

    course_00/quiz_app: save progress

---
+++ course_00/quiz_app/lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:quiz_app/start_screen.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        body: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              colors: [
+                Color.fromARGB(255, 73, 24, 159),
+                Color.fromARGB(255, 100, 41, 201),
+              ],
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+            ),
+          ),
+          child: const StartScreen(),
+        ),
+      ),
+    ),
+  );
+}
--- devenv.nix
+++ devenv.nix
@@ -1,3 +1,4 @@
+{ pkgs, ... }:
 {
   android = {
     enable = true;
@@ -6,5 +7,17 @@
     googleTVAddOns.enable = false;
     android-studio.enable = false;
     flutter.enable = true;
+  };
+
+  enterShell = ''
+    # Prevents the error: (Xcode): Unknown option: -Xlinker
+    # https://github.com/NixOS/nixpkgs/blob/b0d1fe9a32113e0269fae0ea6dd00e5604ccf6a9/pkgs/development/mobile/xcodeenv/build-app.nix#L92
+    export LD="/usr/bin/clang"
+  '';
+
+  stdenv = pkgs.stdenv.override {
+    # Prevents the DEVELOPER_DIR variable from being overwritten
+    # https://github.com/cachix/devenv/issues/1674
+    extraBuildInputs = [ ];
   };
 }
