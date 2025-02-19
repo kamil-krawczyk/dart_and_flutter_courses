da77a056906139fb555051142d389e8c480c4b3a
Kamil Krawczyk <kamil.krawczyk87@gmail.com>
2025-02-19 22:20:01 +0100

    course_00/quiz_app: save progress

---
+++ course_00/quiz_app/lib/questions_screen.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+import 'package:quiz_app/answer_button.dart';
+
+class QuestionsScreen extends StatefulWidget {
+  const QuestionsScreen({super.key});
+
+  @override
+  State<QuestionsScreen> createState() {
+    return _QuestionsScreen();
+  }
+}
+
+class _QuestionsScreen extends State<QuestionsScreen> {
+  @override
+  Widget build(context) {
+    return SizedBox(
+      width: double.infinity,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          const Text(
+            'The question...',
+            style: TextStyle(
+              color: Colors.white,
+              fontSize: 20,
+            ),
+          ),
+          const SizedBox(height: 30),
+          AnswerButton(
+            answerText: 'Answer text...',
+            onTap: () {},
+          ),
+        ],
+      ),
+    );
+  }
+}
