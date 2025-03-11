5cc66b126f5d1aa91ca622f48c0b03e7e1a465e6
Kamil Krawczyk <kamil.krawczyk87@gmail.com>
2025-03-11 22:26:34 +0100

    expense_tracker_app - WIP

--- course_00/expense_tracker_app/lib/main.dart
+++ course_00/expense_tracker_app/lib/main.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
 
-import 'package:expense_tracker_app/expenses.dart';
+import 'package:expense_tracker_app/widgets/expenses.dart';
 
 void main() {
   runApp(
     MaterialApp(
+      theme: ThemeData(useMaterial3: true),
       home: const Expenses(),
     ),
   );
---
+++ course_00/expense_tracker_app/lib/widgets/new_expense.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+
+class NewExpense extends StatefulWidget {
+  const NewExpense({super.key});
+
+  @override
+  State<NewExpense> createState() {
+    return _NewExpenseState();
+  }
+}
+
+class _NewExpenseState extends State<NewExpense> {
+  var _entredTitle = '';
+
+  void _saveTitleInput(String inputValue) {
+    _entredTitle = inputValue;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.all(16),
+      child: Column(
+        children: [
+          TextField(
+            onChanged: _saveTitleInput,
+            maxLength: 50,
+            keyboardType: TextInputType.text,
+            decoration: const InputDecoration(
+              label: Text('Title'),
+            ),
+          ),
+          Row(
+            children: [
+              ElevatedButton(
+                onPressed: () {
+                  print(_entredTitle);
+                },
+                child: const Text(
+                  'Save Expense',
+                ),
+              )
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
