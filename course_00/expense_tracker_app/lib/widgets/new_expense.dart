d4d200b447ec0917aec440e81e1a1fafe810fab8
Kamil Krawczyk <kamil.krawczyk87@gmail.com>
2025-05-29 22:09:42 +0200

    Update dart packages

--- course_00/expense_tracker_app/lib/widgets/new_expense.dart
+++ course_00/expense_tracker_app/lib/widgets/new_expense.dart
@@ -35,7 +35,7 @@ class _NewExpenseState extends State<NewExpense> {
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: EdgeInsets.all(16),
+      padding: const EdgeInsets.all(16),
       child: Column(
         children: [
           TextField(
